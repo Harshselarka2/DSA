e64d3823e111d96ff0785bb29a2111f19db073a4
Harsh selarka <harshselarka2@gmail.com>
2025-05-02 08:46:26 +0530

    simple opration on numbers

---
+++ 30_4_2025/Basics of Language/simple oprations on numbers/num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+int a = 10;
+int b = 20;
+float c = 25.5;
+bool d = true;
+
+printf("sum of a and b is %d \n",a+b);
+printf("sum of a and b is %f \n",a+c);
+printf("%d",d);
+
+  return 0;
+}
