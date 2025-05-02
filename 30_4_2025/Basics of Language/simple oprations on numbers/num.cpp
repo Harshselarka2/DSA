835e6d56cc892ec5ed9a3900a8d3a1d8e28df412
Harsh selarka <harshselarka2@gmail.com>
2025-05-02 08:57:02 +0530

    numbers

---
+++ 30_4_2025/Basics of Language/simple oprations on numbers/num.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a=10;
+    int b=20;
+    float c =25.5;
+    bool d = true;
+
+
+    cout<<"a+b is "<< a+b<<endl;
+    cout<<d<<endl;
+    cout<<a+c;
+    return 0;
+}
---
+++ 30_4_2025/Basics of Language/simple oprations on numbers/num.py
@@ -0,0 +1,12 @@
+def main():
+    a = 10
+    b = 20
+    c = 25.5
+    d = True
+
+    print("a+b is", a + b)
+    print(d)
+    print(a + c)
+
+if __name__ == "__main__":
+    main()
